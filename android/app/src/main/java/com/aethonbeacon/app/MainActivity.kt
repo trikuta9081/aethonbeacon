a6bc7864a807e7bd15a3c95f0b1679661e3a8571
Rajeshwar <trikuta9081@gmail.com>
2026-07-11 14:07:29 +0530

    android: rename package com.aethon.beacon → com.aethonbeacon.app (match Play Console registration)

--- android/app/src/main/java/com/aethonbeacon/app/MainActivity.kt
+++ android/app/src/main/java/com/aethonbeacon/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.aethon.beacon
+package com.aethonbeacon.app
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/aethonbeacon/app/MainApplication.kt
+++ android/app/src/main/java/com/aethonbeacon/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.aethon.beacon
+package com.aethonbeacon.app
 
 import android.app.Application
 import android.content.res.Configuration
